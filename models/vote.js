f17126ca826c632d7ba3ad2ad5d6ba63146e538f
sitakura <scot.itakura@gmail.com>
2020-08-29 13:33:25 -0700

    model-votes

---
+++ models/vote.js
@@ -0,0 +1,36 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+class vote extends Model {}
+
+vote.init(
+  {
+    id: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      autoIncrement: true,
+    },
+    recipe_id : {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      references: {
+        model: "recipe",
+        key: "id",
+      }
+    },
+    user_id: {
+      type: DataTypes.INTEGER,
+      references:{
+        model: "user",
+        key: "id",
+      }
+    },
+    sequelize,
+    freezeTableName: true,
+    timestamps: false,
+    underscored:true,
+    modelName: 'vote'
+  }
+);
+
+module.exports = vote;
