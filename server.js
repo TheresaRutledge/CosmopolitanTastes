48910c66b5784e5e641948d76d9136e85349c1be
sitakura <scot.itakura@gmail.com>
2020-08-31 12:33:33 -0700

    handlebars update

--- server.js
+++ server.js
@@ -3,6 +3,7 @@ const express = require('express');
 const routes = require('./controllers');
 const sequelize = require('./config/connection');
 const exphbs = require('express-handlebars');
+const hbs = exphbs.create({});
 const session = require('express-session');
 const SequelizeStore = require('connect-session-sequelize')(session.Store);
 
@@ -21,7 +22,7 @@ const sess = {
 const app = express();
 const PORT = process.env.PORT || 3001;
 
-// app.engine('handlebars',hbs.engine);
+app.engine('handlebars',hbs.engine);
 app.set('view engine', 'handlebars');
 
 app.use(express.json());
@@ -36,4 +37,4 @@ app.use(routes);
 // turn on connection to db and server
 sequelize.sync({ force: false }).then(() => {
   app.listen(PORT, () => console.log('Now listening'));
-});
+})
