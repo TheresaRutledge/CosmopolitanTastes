c76e95f03a6fdb4a25fc76782018cfcfd178c2a5
Theresa Rutledge <t.roserutledge@gmail.com>
2020-08-31 19:32:40 -0700

    login logout routes

--- controllers/api/users-routes.js
+++ controllers/api/users-routes.js
@@ -111,7 +111,41 @@ router.delete('/:id', (req, res) => {
 });
 
 //login
+router.post('/login', (req, res) => {
+    User.findOne({
+      where: {
+        username: req.body.username
+      }
+    }).then(userData => {
+      if (!userData) {
+        res.status(400).json({ message: 'No user with that username!' });
+        return;
+      }
+      const validPassword = userData.checkPassword(req.body.password);
+      if (!validPassword) {
+        res.status(400).json({ message: `Incorrect Password!` });
+        return;
+      }
+      req.session.save(()=> {
+        req.session.user_id = userData.id;
+        req.session.username = userData.username;
+        req.session.loggedIn = true;
+  
+        res.json({ user: userData, message: 'You arelogged in!' });
+      });
+    });
+  });
 
 //logout
+router.post('/logout',(req,res)=>{
+  if (req.session.loggedIn) {
+    req.session.destroy(() => {
+      res.status(204).end();
+    });
+  }
+  else {
+    res.status(404).end();
+  }
+});
 
 module.exports = router;
