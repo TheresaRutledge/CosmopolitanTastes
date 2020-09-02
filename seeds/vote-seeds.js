9012d0775f0b823cdffa8839f278e233c1b8c69d
rhw <ruohanw89@gmail.com>
2020-09-01 20:37:38 -0600

    update vote and recipe seeds

--- seeds/vote-seeds.js
+++ seeds/vote-seeds.js
@@ -3,47 +3,79 @@ const { Vote } = require("../models");
 const voteData = [
   {
     recipe_id: 1,
-    user_id: [1, 2, 4]
+    user_id: 1
+  },
+  {
+    recipe_id: 1,
+    user_id: 2
+  },
+  {
+    recipe_id: 1,
+    user_id: 4
   },
   {
     recipe_id: 2,
-    user_id: [3]
+    user_id: 3
   },
   {
     recipe_id: 3,
-    user_id: [2, 3]
+    user_id: 2
+  },
+  {
+    recipe_id: 3,
+    user_id: 3
   },
   {
     recipe_id: 4,
-    user_id: [1, 3, 4]
+    user_id: 1
+  },
+  {
+    recipe_id: 4,
+    user_id: 3
+  },
+  {
+    recipe_id: 4,
+    user_id: 4
   },
   {
     recipe_id: 5,
-    user_id: [1]
+    user_id: 1
   },
   {
     recipe_id: 6,
-    user_id: [3, 4]
+    user_id: 3
+  },
+  {
+    recipe_id: 6,
+    user_id: 4
   },
   {
     recipe_id: 7,
-    user_id: [2, 4]
+    user_id: 2
+  },
+  {
+    recipe_id: 7,
+    user_id: 4
   },
   {
     recipe_id: 8,
-    user_id: [2]
+    user_id: 2
   },
   {
     recipe_id: 9,
-    user_id: [1, 2, 3]
+    user_id: 1
+  },  
+  {
+    recipe_id: 9,
+    user_id: 2
   },
   {
     recipe_id: 10,
-    user_id: [4]
+    user_id: 4
   },
   {
     recipe_id: 11,
-    user_id: [1]
+    user_id: 1
   },
 ];
 
